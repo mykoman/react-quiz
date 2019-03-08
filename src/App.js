2dffdbaa4a90dc022bf7909b2977f66f2c2a1f2f
Michael Ogbuma <ogbumamichael@gmail.com>
2019-03-08 04:38:41 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+// import PropTypes from 'prop-types';
 
 class App extends Component {
+  constructor(props)
+  {
+    super(props);
+    this.state = {
+      username : 'mykoman',
+      userData : [],
+      userRepos : [],
+      perPage : 5
+    }
+  }
+
+  getUserData()
+  {
+    $.ajax({
+      url: 'https://api.github.com/users/'+this.state.username+ '?client_id='+this.props.clientId+'&client_secret='+this.props.clientSecret,
+      dataType: 'json',
+      cache: false,
+      success:function(data)
+      {
+        console.log(data); 
+      }.bind(this),
+      error: function(xhr, status, err)
+      {
+        alert(err);
+      }.bind(this)
+
+    })
+  }
+  
+  componentDidMount(){
+    this.getUserData();
+  }
+  
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        {<header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
@@ -17,12 +51,27 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            {this.props.clientId}
           </a>
-        </header>
+        </header>}
       </div>
     );
   }
+
+
 }
+// App.propTypes = {
+//   clientId: React.PropTypes.string,
+//   clientSecret: React.PropTypes.string
+// };
+
+App.defaultProps = {
+  clientId : 'a5a2d6cf3cf8b56ff7e0',
+  clientSecret : 'bf451b95ba56b2f5faeecdea1a9f5c5612e2adc3'
+};
+// App.myke = {
+//   name: "Michael",
+//   gender: 'male'
+// }
 
 export default App;
